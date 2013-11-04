510fc1f034ff0e6a0af480bdff919f6268976329
Natasha Hull <musicalnat@gmail.com>
2013-11-04 12:48:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackUnderflow::Application.config.secret_token = '8974662c4275f5da10442c8476c1d315d4086360e2fb0772ea3941417606246fa769046e1c67eee8e77d546fdc74b8f710c877a8c3848a551f6fe43dda7b1ee2'
