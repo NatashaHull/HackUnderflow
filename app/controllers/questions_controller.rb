6a6a45eaf3b9e27647ac61d932abca92004c74ed
Natasha Hull <musicalnat@gmail.com>
2013-11-05 10:54:50 -0800

    Wrote initial version of questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,59 @@
 class QuestionsController < ApplicationController
+  before_filter :require_current_user!, :except => [:index, :show]
+  before_filter :require_question_owner!, :only => [:edit, :update, :destroy]
+
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    @question.user_id = current_user.id
+
+    if @question.save
+      redirect_to @question
+    else
+      flash.now[:errors] = @question.errors.full_messages
+      render :new
+    end
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
+
+  def update
+    @question = @question
+
+    if @question.update_attributes(params[:question])
+      redirect_to @question
+    else
+      flash.now[:errors] = @question.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_url
+  end
+
+  private
+
+    def require_question_owner!
+      @question = Question.find(params[:id])
+
+      unless @question.user_id == current_user.id
+        flash[:errors] = ["You cannot edit another person's question"]
+        redirect_to @question
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 HackUnderflow::Application.routes.draw do
-  get "questions/index"
-
-  get "questions/show"
-
-  get "questions/new"
-
-  get "questions/edit"
-
   resources :users, :except => [:index, :destroy]
   resource :session, :only => [:new, :create, :destroy]
 
