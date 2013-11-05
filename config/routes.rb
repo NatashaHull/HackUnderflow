e2a4f33bc866623d3ae0865d8c449b673e1016bb
Natasha Hull <musicalnat@gmail.com>
2013-11-05 10:47:22 -0800

    Started working on questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 HackUnderflow::Application.routes.draw do
+  get "questions/index"
+
+  get "questions/show"
+
+  get "questions/new"
+
+  get "questions/edit"
+
   resources :users, :except => [:index, :destroy]
   resource :session, :only => [:new, :create, :destroy]
+
+  resources :questions
 end
