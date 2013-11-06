392513566166a6691f27046455386ad5e3ea1c88
Natasha Hull <musicalnat@gmail.com>
2013-11-05 17:39:06 -0800

    Fixed typo in model name

--- app/models/votes.rb
+++
@@ -1,7 +0,0 @@
-class Votes < ActiveRecord::Base
-  validates_presence_of :direction, :user_id, :voteable_id,
-                        :voteable_type
-
-  belongs_to :voteable, :polymorphic => true
-  belongs_to :user
-end
