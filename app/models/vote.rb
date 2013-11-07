692b248ae56095c2b7c58d12135d25216f6350b6
Natasha Hull <musicalnat@gmail.com>
2013-11-07 08:40:11 -0800

    Deleted depricated method from vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -33,18 +33,4 @@ class Vote < ActiveRecord::Base
     vote.user_id = u_id
     vote.save!
   end
-
-  private
-
-    def cleanup_old_votes
-      old_vote = Vote.find_by_user_id_and_voteable_id_and_voteable_type(
-        self.user_id,
-        self.voteable_id,
-        self.voteable_type
-        )
-
-      unless old_vote.direction == self.direction
-        old_vote.destroy
-      end
-    end
 end
