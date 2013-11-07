49ce66c9a7cce01a1032bdc3761d4ce091a24c84
Natasha Hull <musicalnat@gmail.com>
2013-11-06 17:16:46 -0800

    Fixed bug

--- app/controllers/edit_suggestions_controller.rb
+++ app/controllers/edit_suggestions_controller.rb
@@ -1,5 +1,6 @@
 class EditSuggestionsController < ApplicationController
   before_filter :require_current_user!, :except => [:show]
+  before_filter :require_editable_owner!, :except => [:show]
 
   def show
     @suggestion = EditSuggestion.find(params[:id])
@@ -17,7 +18,7 @@ class EditSuggestionsController < ApplicationController
 
   private
 
-    def require_editable_owner
+    def require_editable_owner!
       @suggestion = EditSuggestion.includes(:editable)
                                   .find(params[:edit_suggestion_id])
       unless @suggestion.editable.user_id == current_user.id
