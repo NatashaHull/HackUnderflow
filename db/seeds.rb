0debf8732d97a316b05cba323211529854744990
Natasha Hull <musicalnat@gmail.com>
2013-11-08 17:40:49 -0800

    Added some fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def create_new_user(username, password)
+  u = User.new(
+    :username => username,
+    :password => password,
+    :password_confirmation => password
+    )
+
+  u.set_session_token
+  u.save!
+  u
+end
+
+users = []
+100.times do
+  users << create_new_user(
+    Faker::Name.name,
+    SecureRandom.urlsafe_base64(16)
+    )
+end
+
+users.each do |user|
+  num = rand(5000)
+  user.add_points(num)
+end
