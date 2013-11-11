8de08b3b0aa3d6c2d0a96f09e32df77a9dab93c8
Natasha Hull <musicalnat@gmail.com>
2013-11-10 18:26:27 -0800

    finished toggling stuff on users#show

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
   $("#profile-answers-0").removeClass("hidden");
   $('.more-button').on("click", function(e) {
     var $userAttr = $(e.currentTarget).parent();
-    objClass = $userAttr.attr("class");
-    objNum = parseInt($userAttr.attr("data-id"));
-    showNextItem(objClass, objNum);
+    var objClass = $userAttr.attr("class");
+    var objNum = parseInt($userAttr.attr("data-id"));
+    var last = parseInt($userAttr.attr("data-last"));
+    showNextItem(objClass, objNum, last);
   });
 
-  var showNextItem = function(objClass, num) {
-    currentItemId = objClass + "-" + num;
-    nextItemId = objClass + "-" + (num+1);
+  var showNextItem = function(objClass, num, last) {
+    if(num === last) {
+      var nextItemId = objClass + "-" + 0;
+    } else {
+      var nextItemId = objClass + "-" + (num+1);
+    }
+
+    var currentItemId = objClass + "-" + num;
     $("#" + currentItemId).addClass("hidden");
     $("#" + nextItemId).removeClass("hidden");
   }
