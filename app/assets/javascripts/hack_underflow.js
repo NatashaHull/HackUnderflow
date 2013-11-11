54163adefd89d38a9d1575c9d7b16e3c3f176916
Natasha Hull <musicalnat@gmail.com>
2013-11-11 10:06:22 -0800

    Changed backbone to be js

---
+++ app/assets/javascripts/hack_underflow.js
@@ -0,0 +1,13 @@
+window.HackUnderflow = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  HackUnderflow.initialize();
+});
--- app/assets/javascripts/hack_underflow.js.coffee
+++
@@ -1,9 +0,0 @@
-window.HackUnderflow =
-  Models: {}
-  Collections: {}
-  Views: {}
-  Routers: {}
-  initialize: -> alert 'Hello from Backbone!'
-
-$(document).ready ->
-  HackUnderflow.initialize()
